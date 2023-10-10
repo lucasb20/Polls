0be7fc1fc78fc646602fa28b2fd9ac1054aac9a5
Lucas Rocha <lr359508@gmail.com>
2023-10-10 07:45:05 -0300

    Bug da view Results resolvido.
    Após votar, o client era para ser redirecionado para a página de resultados dos votos. O código de redirect estava com erro de sintaxe, mas já foi resolvido.

--- polls/views.py
+++ polls/views.py
@@ -62,5 +62,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
 
-
-    return HttpResponseRedirect(reverse("polls:results", args=(question_id)))
+    return HttpResponseRedirect(reverse("polls:results", args=[question_id,]))
